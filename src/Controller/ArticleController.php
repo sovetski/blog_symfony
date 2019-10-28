ee1566419a49098237b9e85fa56baac0e28cb9cb
Bryan <bryanlmei@laposte.net>
2019-10-28 15:01:54 +0100

    Afficher article sur show()

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,8 +11,6 @@ class ArticleController extends AbstractController
 {
     /**
      * @Route("/blog")
-     * @param ArticleRepository $repository
-     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function index(ArticleRepository $repository)
     {
@@ -19,6 +18,16 @@ class ArticleController extends AbstractController
 
         return $this->render('article/index.html.twig', [
             'articles' => $articles,
+        ]);
+    }
+
+    /**
+     * @Route("/article/{id}", methods="GET")
+     */
+    public function show(Article $article)
+    {
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
         ]);
     }
 }
